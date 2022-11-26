1ba8b344a1bfa2fcc68548ba3142597ba15cbbb2
Piyush Yadav <pyadav2@ce.iitr.ac.in>
2022-11-26 14:07:12 +0530

    Better Responsive

--- js/commands.js
+++ js/commands.js
@@ -7,7 +7,7 @@ banner = [
   "╚═╝     ╚═╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝  ╚═══╝",
 ];
 
-const error=`<p id="error">Command not found.</p><br>`;
+const error=`<span id="error">Command not found.</span>`;
 
 const subtext=`For a list of available commands type <span class="cmd-deco" id="helper">'help'</span>`;
 
@@ -73,24 +73,24 @@ const education=`
 const projects=`
   <ul>
     <li><strong class="cmd-deco">Xflix | Crio.Do</strong> - Build backend for a video sharing platform using Node.js. Implemented the functionality of searching by title, filtering using Genre and Content Rating including sorting by Views and Release Date.<br>
-    <b>Tech Stack: Node.js, Express.js, MongoDb, Mongoose</b></li>
+    <b>Tech Stack: Node.js, Express.js, MongoDb, Mongoose</b></li><br>
     <li><strong class="cmd-deco">Qtrip | Crio.Do</Strong> - Created 3 different web pages for a travelling web app using HTML, CSS and Bootstrap and made them dynamic using Javascript.<br>
-    <b>Tech Stack: HTML, CSS, JavaScript, Bootstrap, REST APIs, DOM Manipulation</b></li>
+    <b>Tech Stack: HTML, CSS, JavaScript, Bootstrap, REST APIs, DOM Manipulation</b></li><br>
     <li><strong class="cmd-deco">Travel Behaviour Modelling | IIT Roorkee</strong> - Conducted a study to see the implementation of car-free days in the campus. <br>Formed a regression equation using MATLAB for predicting the driving factors for people's choices. Created a Heatmap to showcase the findings.</li>
   </ul>`;
 
 const experience=`
 <div>
   <b class="cmd-deco">Jio Platforms | SDE-1 (July 2022 - Present)</b><br>
-  <p> -> Worked on the development of Coupon Store Web Application using Spring Boot which is a unified platform to add, update and activate different coupons.<br>
-  -> Worked in invenotry management team of JioMart which provided web applications to push and update items.<br>
-  Tech Stack: Spring Boot, MySql</p>
+  <p> -> Worked on the development of Coupon Store Web Application using Spring Boot which is a unified platform to add, update and activate different coupons.<br><br>
+  -> Worked in invenotry management team of JioMart which provided web applications to push and update items.<br><br>
+  <b>Tech Stack:</b> Spring Boot, MySql</p>
 </div>`;
 
 const skills=`
 <div>
   <ul>
-    <li><b>Languages: </b>Java, C++, JavaScript, HTML, CSS</li>
+    <li><b>Languages: </b>Java, C++, JavaScript, HTML, CSS</li><br>
     <li><b>Software Packages: </b>Spring Boot, Node.js, Express.js, MySQL, MongoDb, Mongoose, Git</li>
   </ul>
 </div>`;
@@ -98,6 +98,7 @@ const skills=`
 const contact=`
 <div>
 -><b>Email: </b><a href="mailto:pyadav2@ce.iitr.ac.in" target="_blank">pyadav2@ce.iitr.ac.in</a><br>
+<br>
 -><b>LinkedIn: </b><a href="https://www.linkedin.com/in/piyush-yadav-a2590416a/" target="_blank">https://www.linkedin.com/in/piyush-yadav-a2590416a/</a><br>
 </div>`;
 
